0ce101c86840bdd78353c108ae3bf7b73d00c1ef
codeswithankit <ank8804465648@gmail.com>
2020-07-05 09:08:42 +0530

    Serving static file

--- mod.ts
+++ mod.ts
@@ -1,15 +1,41 @@
-import { Application } from "https://deno.land/x/oak@v5.0.0/mod.ts";
+import { Application, send } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 
 const app = new Application();
 
 const PORT = 8000;
+
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png",
+  ];
+  await send(ctx, filePath, {
+    root: `${Deno.cwd()}/public`,
+  });
+});
 
 app.use((ctx) => {
   ctx.response.body = `Nasa mission control project`;
 });
 
 if (import.meta.main) {
-  app.listen({
+  await app.listen({
     port: PORT,
   });
 }
